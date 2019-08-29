501ab51d835c52f34ca10f02355402e4a07dfd6c
Simon Frost <simonhfrost@gmail.com>
2019-08-29 18:42:26 +0200

    Small design changes

--- header.php
+++ header.php
@@ -2,6 +2,5 @@
     <a href="/"><img class="logo" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.svg"/></a>
     <div>
         <h1>ANTISEMANTIC</h1>
-        <h3>Raven mit Anstand und Würde</h3>
     </div>
 </header>
