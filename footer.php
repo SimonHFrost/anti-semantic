db6dcbf41b06c42ce5e6866060d6a99d7f1485d6
Simon Frost <simonhfrost@gmail.com>
2019-08-29 12:05:59 +0200

    Add manifesto to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <footer>
     <div class="spacer"></div>
+    <a class="footer-link" href="manifesto">Manifesto</a>
     <a class="footer-link" href="#">Impressum</a>
     <a class="footer-link" href="#">Datenschutz</a>
 </footer>
