db91590eb1cdda5329090ac1ea930f65a757fd0b
Simon Frost <simonhfrost@gmail.com>
2019-08-29 11:52:34 +0200

    Extract head

---
+++ head.php
@@ -0,0 +1,14 @@
+<head>
+    <title>ANTISEMANTIC</title>
+    <meta charset="utf-8" /> 
+    <link rel="shortcut icon" href="<?php echo get_bloginfo('template_directory'); ?>/images/logo.svg">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/css/style.css" rel="stylesheet">
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/lib/normalize.css" rel="stylesheet">
+
+    <?php wp_head(); ?>
+</head>
--- index.php
+++ index.php
@@ -1,17 +1,4 @@
-<head>
-    <title>ANTISEMANTIC</title>
-    <meta charset="utf-8" /> 
-    <link rel="shortcut icon" href="<?php echo get_bloginfo('template_directory'); ?>/images/logo.svg">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-    
-    <link rel="icon" type="image/png" href="favicon.png">
-    <link href="<?php echo get_bloginfo('template_directory'); ?>/css/style.css" rel="stylesheet">
-    <link href="<?php echo get_bloginfo('template_directory'); ?>/lib/normalize.css" rel="stylesheet">
-
-    <?php wp_head(); ?>
-</head>
+<?php get_template_part( 'head' ); ?>
 <body>
     <div class="main-content">
         <?php get_template_part( 'header' ); ?>
