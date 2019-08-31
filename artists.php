6a0324daaf021987d51eb375ed8abaa9cb78c2bf
Simon Frost <simonhfrost@gmail.com>
2019-08-31 10:29:55 +0200

    format artist entries

--- artists.php
+++ artists.php
@@ -9,29 +9,29 @@
             <main>
                 <h2>Artists</h2>
                 <div class="artist-grid">
-                    <a href="/artists/artist1" class="artist-box">
+                    <a href="/artists#artist1" class="artist-box">
                         <img class="artist-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/artist1.jpg" />
-                        <h3>Simon Frost</h3>
+                        <h3 class="artist-name">Simon Frost</h3>
                     </a>
-                    <a href="/artists/artist2" class="artist-box">
+                    <a href="/artists#artist2" class="artist-box">
                         <img class="artist-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/artist1.jpg" />
-                        <h3>Simon Frost</h3>
+                        <h3 class="artist-name">Simon Frost</h3>
                     </a>
-                    <a href="/artists/artist3" class="artist-box">
+                    <a href="/artists#artist3" class="artist-box">
                         <img class="artist-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/artist1.jpg" />
-                        <h3>Simon Frost</h3>
+                        <h3 class="artist-name">Simon Frost</h3>
                     </a>
-                    <a href="/artists/artist4" class="artist-box">
+                    <a href="/artists#artist4" class="artist-box">
                         <img class="artist-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/artist1.jpg" />
-                        <h3>Simon Frost</h3>
+                        <h3 class="artist-name">Simon Frost</h3>
                     </a>
-                    <a href="/artists/artist5" class="artist-box">
+                    <a href="/artists#artist5" class="artist-box">
                         <img class="artist-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/artist1.jpg" />
-                        <h3>Simon Frost</h3>
+                        <h3 class="artist-name">Simon Frost</h3>
                     </a>
-                    <a href="/artists/artist6" class="artist-box">
+                    <a href="/artists#artist6" class="artist-box">
                         <img class="artist-image" src="<?php echo get_bloginfo('template_directory'); ?>/images/artist1.jpg" />
-                        <h3>Simon Frost</h3>
+                        <h3 class="artist-name">Simon Frost</h3>
                     </a>
                 </div>
             </main>
