21094923a4e5068ab116c211e8315d5790f78332
Simon Frost <simonhfrost@gmail.com>
2019-09-05 12:01:59 +0200

    Fix issues with functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
-<!-- url loosely matches categories and is used for filtering -->
-
 <?php
+    /* url loosely matches categories and is used for filtering */
+
     function getUrl() {
+        return 'past';
         if(strpos($_SERVER['REQUEST_URI'], 'past') !== false){
             return 'past';
         } else {
