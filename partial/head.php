c7074d51eafd5ed070196339594c9193e0b6daa1
Simon Frost <simonhfrost@gmail.com>
2019-09-12 12:05:46 +0200

    Extract js to seperate file

--- partial/head.php
+++ partial/head.php
@@ -9,34 +9,10 @@
     <link rel="icon" type="image/png" href="favicon.png">
 
     <link href="<?php echo get_bloginfo('template_directory'); ?>/lib/normalize.css" rel="stylesheet">
-    <link href="<?php echo get_bloginfo('template_directory'); ?>/css/base.css" rel="stylesheet">
-    <link href="<?php echo get_bloginfo('template_directory'); ?>/css/artists.css" rel="stylesheet">
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/assets/base.css" rel="stylesheet">
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/assets/artists.css" rel="stylesheet">
     
-    <script>
-        // Script to dynamically load theme based on query params 
-
-        function getQueryVariable(variable) {
-            var query = window.location.search.substring(1);
-            var vars = query.split('&');
-            for (var i = 0; i < vars.length; i++) {
-                var pair = vars[i].split('=');
-                if (decodeURIComponent(pair[0]) == variable) {
-                    return decodeURIComponent(pair[1]);
-                }
-            }
-            console.log('Query variable %s not found', variable);
-        }
-
-        var currentTheme = getQueryVariable('theme') || style1;
-        var themeString = `/css/${getQueryVariable('theme')}.css`;
-
-        var fileref = document.createElement("link")
-        fileref.setAttribute("rel", "stylesheet")
-        fileref.setAttribute("type", "text/css")
-        fileref.setAttribute("href", `<?php echo get_bloginfo('template_directory'); ?>${themeString}`)
-        document.getElementsByTagName("head")[0].appendChild(fileref)
-    </script>
-    
+    <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/theme.js"></script>
 
     <?php wp_head(); ?>
 </head>
