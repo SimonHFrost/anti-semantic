2e04fef9d69aef1472acc32b1f9297594b722ce9
Simon Frost <simonhfrost@gmail.com>
2019-09-13 16:05:31 +0200

    Add glitch logo

--- partial/head.php
+++ partial/head.php
@@ -1,7 +1,7 @@
 <head>
     <title>ANTISEMANTIC</title>
     <meta charset="utf-8" /> 
-    <link rel="shortcut icon" href="<?php echo get_bloginfo('template_directory'); ?>/images/logo.svg">
+    <link rel="shortcut icon" href="<?php echo get_bloginfo('template_directory'); ?>favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
--- partial/header.php
+++ partial/header.php
@@ -1,5 +1,5 @@
 <header>
-    <a href="/"><img class="logo" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo.svg"/></a>
+    <a href="/"><img class="logo" src="<?php echo get_bloginfo('template_directory'); ?>/images/logo-animated.gif"/></a>
     <div>
         <h1>ANTISEMANTIC</h1>
     </div>
