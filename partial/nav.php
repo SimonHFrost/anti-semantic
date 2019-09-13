3fdfdb5f4a0c9d58d08420d7a1d5098dcebbb584
Simon Frost <simonhfrost@gmail.com>
2019-09-13 15:25:18 +0200

    Fix missing url

--- partial/nav.php
+++ partial/nav.php
@@ -1,8 +1,8 @@
 <nav>
-    <a class="folder-link <?php if (getUrl() === 'upcoming') { echo 'active'; } ?>" href="/">
+    <a class="folder-link active'" href="/">
         <img class="folder" src="<?php echo get_bloginfo('template_directory'); ?>/images/folder.png"/><label>Upcoming</label>
     </a>
-    <a class="folder-link <?php if (getUrl() === 'past') { echo 'active'; } ?>" href="/past">
+    <a class="folder-link" href="/past">
         <img class="folder" src="<?php echo get_bloginfo('template_directory'); ?>/images/folder.png"/><label>Past</label>
     </a>
 </nav>
