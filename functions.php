3aa4f79537f52c8b8cff2dd422789a8114313a21
Simon Frost <simonhfrost@gmail.com>
2019-09-13 15:20:31 +0200

    Remove url section in functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,4 @@
 <?php
-    /* url loosely matches categories and is used for filtering */
-
-    function getUrl() {
-        if(strpos($_SERVER['REQUEST_URI'], 'past') !== false){
-            return 'past';
-        } else {
-            return 'upcoming';
-        }
-    }
-?>
-
-<?php
 add_filter('single_template', 'check_for_category_single_template');
 function check_for_category_single_template( $t )
 {
