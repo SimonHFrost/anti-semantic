840cb104a1a94127559cdc72cc873e7bb497dd64
Simon Frost <simonhfrost@gmail.com>
2019-09-26 19:09:04 +0200

    email link in footer

--- partial/footer.php
+++ partial/footer.php
@@ -1,5 +1,6 @@
 <footer>
     <a class="footer-link mobile-only" href="mailto:mail@antisemantic.de">mail@antisemantic.de</a>
+    <a class="footer-link desktop-only" href="mailto:mail@antisemantic.de">@</a>
     <div class="spacer"></div>
     <a class="footer-link" href="/manifesto">Manifesto</a>
     <a class="footer-link" href="/impressum">Impressum</a>
