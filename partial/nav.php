1cd245fdf9c3114ccf9c903acd0d8892a673a93f
Simon Frost <simonhfrost@gmail.com>
2019-09-26 19:15:09 +0200

    Add artists to nav bar

--- partial/nav.php
+++ partial/nav.php
@@ -1,10 +1,14 @@
 <nav>
-    <a class="folder-link <?php if (strpos($_SERVER['REQUEST_URI'], 'past') === false) { echo 'active'; } ?>" href="/">
+    <a class="folder-link <?php if ($_SERVER['REQUEST_URI'] === '/') { echo 'active'; } ?>" href="/">
         <div class="folder"></div>
         <label>Upcoming</label>
     </a>
-    <a class="folder-link <?php if (strpos($_SERVER['REQUEST_URI'], 'past') !== false) { echo 'active'; } ?>" href="/past">
+    <a class="folder-link <?php if (strpos($_SERVER['REQUEST_URI'], 'past')) { echo 'active'; } ?>" href="/past">
         <div class="folder"></div>
         <label>Past</label>
+    </a>
+    <a class="folder-link <?php if (strpos($_SERVER['REQUEST_URI'], 'artists')) { echo 'active'; } ?>" href="/artists">
+        <div class="folder"></div>
+        <label>Artists</label>
     </a>
 </nav>
