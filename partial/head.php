659a1ea3f8835d5004010412b8470524deabf8d3
Simon Frost <simonhfrost@gmail.com>
2019-09-26 11:08:36 +0200

    Fix reference to favicon

--- partial/head.php
+++ partial/head.php
@@ -1,7 +1,7 @@
 <head>
     <title>ANTISEMANTIC</title>
     <meta charset="utf-8" /> 
-    <link rel="shortcut icon" href="<?php echo get_bloginfo('template_directory'); ?>favicon.png">
+    <link rel="shortcut icon" href="<?php echo get_bloginfo('template_directory'); ?>/favicon.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
@@ -12,7 +12,5 @@
     <link href="<?php echo get_bloginfo('template_directory'); ?>/assets/base.css" rel="stylesheet">
     <link href="<?php echo get_bloginfo('template_directory'); ?>/assets/artists.css" rel="stylesheet">
     
-    <script src="<?php echo get_bloginfo('template_directory'); ?>/assets/theme.js"></script>
-
     <?php wp_head(); ?>
 </head>
