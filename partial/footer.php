7bb98509e1da0737745de95b1f5d121742b5f587
Simon Frost <simonhfrost@gmail.com>
2019-09-26 18:59:07 +0200

    Show contact email in footer

--- partial/footer.php
+++ partial/footer.php
@@ -1,5 +1,6 @@
 <footer>
     <div class="spacer"></div>
+    <a class="footer-link mobile-only" href="mailto:mail@antisemantic.de">mail@antisemantic.de</a>
     <a class="footer-link" href="/manifesto">Manifesto</a>
     <a class="footer-link" href="/impressum">Impressum</a>
     <a class="footer-link" href="/datenschutz">Datenschutz</a>
